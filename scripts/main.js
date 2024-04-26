214667464031fa724b3d37f53b561ff1f3ff2c4e
NethNut <144717664+NethNut@users.noreply.github.com>
2024-04-26 19:03:38 +0000

    added console.log 1-5

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+console.log("Task 1");
+for (let i = 1; i < 11; i++) {
+    console.log(i * i);
+}
+console.log("Task 2");
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log("blast off");
+console.log("Task 3");
+for (let i = 0; i <= 50; i += 2) {
+    console.log(i);
+}
+console.log("Task 4");
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+console.log("Task 5");
+let factorial = 1;
+for (let i = 1; i <= 10; i = i + 1)     {
+    factorial *= i;
+    console.log(factorial);
+}
+console.log("Task 6");
+
+console.log("Task 7");
+
+console.log("Task 8");
+
+console.log("Task 9");
+
+console.log("Task 10");
