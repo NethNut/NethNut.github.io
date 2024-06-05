fef56d340c8b52133561cbab2cd2784409d1dc76
NethNut <144717664+NethNut@users.noreply.github.com>
2024-06-05 17:39:24 +0000

    New runnig button

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-
 function changeBackgroundColor(color) {
   document.body.style.backgroundColor = color;
+  localStorage.setItem("bgColor", color);
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.body.style.backgroundColor = localStorage.getItem("bgColor") ?? "lightblue";
+});
